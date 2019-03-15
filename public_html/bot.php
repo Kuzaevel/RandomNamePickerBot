0254a1fa93c56efe4832faa77c2b8dbd1688ccdc
kuziaev.er <kuziaev.er@mail.ru>
2019-03-15 12:33:16 +0500

    add keyboard

--- public_html/bot.php
+++ public_html/bot.php
@@ -1,20 +1,61 @@
 <?php
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);*/
+error_reporting(E_ALL);
 
 require('../vendor/autoload.php'); //Подключаем библиотеку
 use Telegram\Bot\Api;
 
 $telegram = new Api('');
 $result = $telegram -> getWebhookUpdates();
-$text = $result["message"]["text"]; //Текст сообщения
+$text = $_GET['text']?$_GET['text'] : $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-$keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
+$keyboard_start = [["Списко"],["Выбор из списка"]]; //Клавиатура
+
+$arr = [ "bv1", "имя2","имя3","name", "surname"];
 
 if($text){
-    $reply = "Добро пожаловать, $name";
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+
+    switch ($text) {
+
+        case "rnd":
+            $reply = "";
+
+//            var_dump($arr[array_rand($arr)]);
+//
+            foreach($arr as $val) {
+                //echo $val;
+                $reply .= $val . "\r\n";
+            }
+
+            foreach ($arr as $k=>$v) {
+                echo $v;
+                $reply .= $v."\r\n";
+            }
+
+        break;
+
+        case "/start":
+            $reply_markup = $telegram->replyKeyboardMarkup([
+                'keyboard' => $keyboard_start,
+                'resize_keyboard' => true,
+                'one_time_keyboard' => false
+            ]);
+            $reply = "Добро пожаловать!!!";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
+            break;
+        case "Списко":
+            foreach($arr as $val) {
+                $reply .= $val . ("\r\n");
+            }
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+
+            //$reply_markup = $telegram->
+            break;
+        case "Выбор из списка":
+            $reply = $arr[array_rand($arr)];
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+            break;
+    }
 }
-?>
